a4e5ff45eab8730c3839ae7766dd4aff23cb7978
l4mp1 <l.charissis@gmail.com>
2020-06-14 14:02:49 -0700

    Update mysql_fulltextsearch.php

--- mysql_fulltextsearch.php
+++ mysql_fulltextsearch.php
@@ -1,6 +1,6 @@
 <?php
+//MyISAM Engine
 $searchphrase_extended = "'\"".$searchphrase."\"'";
-  //Zeige einen interpreten
   if ($stmt = $connect->prepare("SELECT column FROM table WHERE MATCH column AGAINST (? IN BOOLEAN MODE)")) {
         $stmt->bind_param("s",$searchphrase_extended);
         $stmt->execute();
@@ -8,4 +8,12 @@ $searchphrase_extended = "'\"".$searchphrase."\"'";
         $stmt->close();
     }
 
+//InndoDB Engine
+$searchphrase_extended = "%".$searchphrase."%";
+  if ($stmt = $connect->prepare("SELECT column FROM table WHERE column LIKE ?")) {
+        $stmt->bind_param("s",$searchphrase_extended);
+        $stmt->execute();
+        $stmt->bind_result($result);
+        $stmt->close();
+    }
 ?>
